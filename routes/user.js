2365a76aceb764b02aefb66cdab8c7d286ba3c71
Ashiqueachi696 <ashique@dataspot.in>
2024-01-28 22:09:35 +0530

    test

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {signUp,logIn} = require('../controller/user_controller')
+const {signUp,logIn,forgotPassword} = require('../controller/user_controller')
 const upload = require('../config/multer_config')
 const cloudinary = require('../config/cloudinary_config')
 router.post('/signUp',upload.single("image"),signUp)
 router.post('/login',logIn)
+router.post('/forgotPassword',forgotPassword)
 module.exports=router
