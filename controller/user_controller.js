fd0dfaf20f1cd0b630e0bb1a97162088f25d8ded
Ashiquekk696 <ashiquesofdevoloper@gmail.com>
2024-05-07 03:06:06 -0700

    new

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -8,17 +8,16 @@ const { NamiPic } = require("../model/nami_img");
 
 
 exports.uploadImage= async (req,res)=>{
+  const {filename,path} =await req.file
   await cloudinary.v2.uploader.upload(path,{folder:"ashique_ecommerce_subfolder"},async(error, cldResult) => {
     console.log(cldResult);
     if(error){
       res.status(500).json({ error: 'Error uploading image to Cloudinary' });
       return;
    
-   
-    }
-    const img = cldResult.secure_url;
+    }   const img = cldResult.secure_url;
   const data = NamiPic({img})
-  await res.status(201).json({mess:"User Created Successfully",result:data})
+  await res.status(201).json({mess:"Image uploaded Successfully",result:data})
   }
   )
 }
